128f563aebb6ddfae6104374f6cd9ad21cd7bb11
lalunofrizalfirmansyah_Alprog-C <firmansayhh05112006@gmail.com>
2026-04-18 23:52:16 +0800

    Add files via upload

---
+++ tugas1if-else.py
@@ -0,0 +1,16 @@
+# if 
+# else
+#elif
+
+nama = input("masukkan nama")
+NIM = input(" masukkan nim")
+
+if nama =="nofrizal" :
+   NIM  =="5"
+   print("nama anda nofrizal")
+   print("NIM anda 25241080")
+else : 
+    print("mohon maaf anda bukan nofrizal")
+    print("nama yang anda masukkan salah")
+    print("NIM anda tidak bisa di akses")
+print("program selesai")
