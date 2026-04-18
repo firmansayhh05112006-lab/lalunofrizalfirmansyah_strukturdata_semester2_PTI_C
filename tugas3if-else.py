2b8bbd7e5b0286c7db40fda0c9fd8c7b51d2bca0
lalunofrizalfirmansyah_Alprog-C <firmansayhh05112006@gmail.com>
2026-04-19 01:23:29 +0800

    Add files via upload

---
+++ tugas3if-else.py
@@ -0,0 +1,23 @@
+# minta user masukan angka
+# jika angka negatif ,minta user masukan angka positif
+
+# contoh:
+# masukan angka: -3
+# harus positif!
+# masukan angka:
+
+# jawaban
+
+while True:
+    angka =int(input("masukan angka yanng anda mau"))
+
+    if angka >= 0:
+        print (f"nilai angka:{angka}")
+        print("allahuakkbaryarobilyakarim")
+
+    else:
+         print("harus positif!")
+         print("silahkan mausukan angka yangg poositif di halaman berikutnya")
+
+    print(f"terimakasih anda memasukkan angka{angka}")
+          
