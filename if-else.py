743c1340a3c111379a4a7f3b1d5d798e03cb1af3
lalunofrizalfirmansyah_Alprog-C <firmansayhh05112006@gmail.com>
2026-04-18 23:51:40 +0800

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,12 @@
+nama = input("masukan nama anda:")
+if nama=="edy" :
+   print("selamat datang edy")
+   print("dosen ganteng")
+elif nama=="dika" :
+   print("mahasiswa ganteng")
+elif nama=="tiara" : 
+   print("mahasiswi cantik")
+else :
+   print("anda bukan dosen/mahasiswa")
+   print("annda tidak gantenng")
+print("program selesai")
