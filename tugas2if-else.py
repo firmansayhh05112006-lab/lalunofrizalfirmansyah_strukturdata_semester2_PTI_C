ec12387ee98d48ca6a3e0de4526f2b4d19da6f2c
lalunofrizalfirmansyah_Alprog-C <firmansayhh05112006@gmail.com>
2026-04-19 01:23:13 +0800

    Add files via upload

---
+++ tugas2if-else.py
@@ -0,0 +1,30 @@
+# soal: 
+# masukkan nama anda ;{nama pendek anda}
+# jika benar akan lanjut ke program selanjutnya 
+# jika salah akan muncul pesan "silahkan coba lagi"
+
+# buat program yang menampilkan tabel perkalian dari angka yang di masukkan user (1,10).
+# contoh: 
+# masukkan angka:3
+# 3x 1= 3
+# 3x 2= 6
+# ...
+# 3 x 1 = 30
+
+# jawaban
+
+namaa= input("masukan nama pendek anda")
+
+if nama== "nofrizal"
+    print("terimakasih telah mengisi nama pendek yang benar")
+    print("silahkan lanjut ke program selanjutnya")
+
+else:
+    print("nama pendek yang anda masukan salah")
+    print("silahkan coba lagi")
+
+angka= int (input("mau lihat  perkalian berapa"))
+
+for i in range (1,11):
+    hasil= angka*i 
+    print(f"{angka}*{i}={hasil}")  
