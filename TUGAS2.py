f4c4a2f4c83f53b2837132442ed178b1a4be5fc1
lalunofrizalfirmansyah_Alprog-C <firmansayhh05112006@gmail.com>
2026-04-19 00:31:04 +0800

    Add files via upload

---
+++ TUGAS2.py
@@ -0,0 +1,22 @@
+# kondisi 
+# aksi
+
+nama  = "nofrizal"
+input = input("masukan nama anda: ")
+
+if input == nama :
+
+    print("jika benar akan lanjut ke program selanjutnya")
+
+    try: 
+        angka = int(input("masukan angka"))
+
+        for i in range(1, 11) :
+             print(f"{"angka"} x {i} = {angka* i}")
+    axcept ValueError
+         pass 
+    print("program selesai")
+else: 
+    print("silahkan coba lagi")
+
+
